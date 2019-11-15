c6721372a060c2f9f4de283edadd76df9766113b
unknown <toyyibat.abdulkareem@gmail.com>
2019-11-15 09:04:41 +0100

    Updates

---
+++ precache-manifest.19c725a8abe5cf374ac175888c361fda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5633be36fda98d2266b76c740dedd179",
+    "url": "/monsters-rolodes/index.html"
+  },
+  {
+    "revision": "492b8a689cd883ab4df9",
+    "url": "/monsters-rolodes/static/css/main.1bcf9da2.chunk.css"
+  },
+  {
+    "revision": "db24dc879035f8631897",
+    "url": "/monsters-rolodes/static/js/2.a47b4d75.chunk.js"
+  },
+  {
+    "revision": "492b8a689cd883ab4df9",
+    "url": "/monsters-rolodes/static/js/main.9bdb834f.chunk.js"
+  },
+  {
+    "revision": "28422a00492803c9cb5e",
+    "url": "/monsters-rolodes/static/js/runtime-main.2b041bc8.js"
+  }
+]);
